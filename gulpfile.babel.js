d0075acef20c6caeb01088967df138178e29eccc
Dima <65868322+byCodeXp@users.noreply.github.com>
2021-06-08 19:25:00 +0300

    gulp dev & gulp watch

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,17 +1,23 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
-const css = require('gulp-clean-css');
+const g_sass = require('gulp-sass');
+const g_css = require('gulp-clean-css');
 
-function style() {
+function sass() {
     return gulp.src('./res/sass/*.scss')
-        .pipe(sass())
+        .pipe(g_sass())
         .pipe(gulp.dest('./public/css'));
 }
 
-function minify() {
+function css() {
     return gulp.src('./public/css/*.css')
-        .pipe(css())
+        .pipe(g_css())
         .pipe(gulp.dest('./public/css'));
 }
 
-exports.default = gulp.series(style, minify);
+function watch() {
+    gulp.watch('./res/sass/*.scss', sass);
+    gulp.watch('./public/css/*.css', css);
+}
+
+exports.dev = gulp.series(sass, css);
+exports.watch = watch;
