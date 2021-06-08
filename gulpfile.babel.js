e6f2bf88cf5dd2f08e8aa35d3a4ba87436b4258f
Dima <65868322+byCodeXp@users.noreply.github.com>
2021-06-08 17:46:12 +0300

    setup sass

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,10 @@
-exports.default = function () {
-    console.log('That\'s working!');
+import gulp from 'gulp';
+import sass from 'gulp-sass';
+
+function style() {
+    return gulp.src('./res/sass/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./public/css'));
 }
+
+exports.style = style;
