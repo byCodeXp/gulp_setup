144fd6de4a91c4ea189ef97f66f40f8c83a79ea7
Dima <65868322+byCodeXp@users.noreply.github.com>
2021-06-08 18:31:21 +0300

    install and setup sass and clean-css

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,17 @@
-exports.default = () => {
-    console.log('That\'s works!');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const css = require('gulp-clean-css');
+
+function style() {
+    return gulp.src('./res/sass/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./public/css'));
 }
+
+function minify() {
+    return gulp.src('./public/css/*.css')
+        .pipe(css())
+        .pipe(gulp.dest('./public/css'));
+}
+
+exports.default = gulp.series(style, minify);
