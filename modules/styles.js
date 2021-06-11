f6106444aaf1a060b812992b2c2c94c9c94a904d
Dima <65868322+byCodeXp@users.noreply.github.com>
2021-06-11 15:59:20 +0300

    some changes

--- modules/styles.js
+++ modules/styles.js
@@ -1,13 +1,12 @@
 import { src, dest } from 'gulp';
 import sass from 'gulp-sass';
 import clean_css from 'gulp-clean-css';
-import { cfg } from '../configuration';
 
 export const styles = () => (
-    src(`${cfg.source}style/*.scss`)
+    src('./res/styles/app.scss')
         .pipe(sass())
-        .pipe(dest(`${cfg.dest}css`))
-        .pipe(src(`${cfg.dest}css/*.css`))
+        .pipe(dest('public/css'))
+        .pipe(src('public/css/*.css'))
         .pipe(clean_css())
-        .pipe(dest(`${cfg.dest}css`))
+        .pipe(dest('./public/css'))
 );
